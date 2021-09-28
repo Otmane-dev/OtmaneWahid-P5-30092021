832e632194974d41718fe4439c683740034efa29
Otmane-dev <83417450+Otmane-dev@users.noreply.github.com>
2021-09-28 17:18:53 +0200

    css

--- commande.js
+++ commande.js
@@ -3,6 +3,6 @@ prixTotal = localStorage.getItem("prixTotal") ;
 
 let commande = document.querySelector("#commande");
 
-commande.innerHTML = "Numero de la commande: "+orderId+" prix total: "+prixTotal+"€";
+commande.innerHTML = "Numero de la commande: "+orderId+"Prix total: "+prixTotal+"€";
 
 console.log(orderId);
