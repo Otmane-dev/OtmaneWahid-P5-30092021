8d6548c2888440cf0a6ae2d2642d04f4c948d44a
Otmane-dev <83417450+Otmane-dev@users.noreply.github.com>
2021-10-04 19:18:22 +0200

    selection d'un seul produit dans pagner

--- produit.js
+++ produit.js
@@ -1,16 +1,16 @@
 let produit = document.querySelector(".produit");
-fetch('http://localhost:3000/api/cameras/'+location.search.split('id=')[1])
+fetch('http://localhost:3000/api/cameras/' + location.search.split('id=')[1])
   .then(response => response.json())
-  .then(data => { 
-      console.log(data);
-     
-          produit.innerHTML += `
+  .then(data => {
+    console.log(data);
+
+    produit.innerHTML += `
           
             <figure>
                 <img src=${data.imageUrl}>
                 <figcaption class="name-price">
                     <div class="name" id="name">${data.name}</div>
-                    <div class="price" id="price">${data.price /100} € </div>
+                    <div class="price" id="price">${data.price / 100} € </div>
                     <div class="lenses" id="lenses">${data.lenses}</div>
                     <div class="description" id="description">${data.description}</div>
                     
@@ -21,47 +21,49 @@ fetch('http://localhost:3000/api/cameras/'+location.search.split('id=')[1])
            
           
           `;
-ajouterAuPanier(data);
-     
+    ajouterAuPanier(data);
+
   });
 
-  function ajouterAuPanier(data)
-  {
-    let btn_envoiPanier = document.getElementById("btn-envoyer");
- 
-    btn_envoiPanier.addEventListener("click", (event)=>{
-        event.preventDefault();
-      
-        let selectProduit = {
-          imageUrl:data.imageUrl,
-          name: data.name,
-          _id: data._id ,
-          quantite: 1,
-          price: data.price /100,
-      }
-      let productsListe = [];
+function ajouterAuPanier(data) {
+  let btn_envoiPanier = document.getElementById("btn-envoyer");
 
-      if (localStorage.getItem("panier") === null) {
-        //...
-        
-        productsListe.push(selectProduit);
-    
-        localStorage.setItem("panier",JSON.stringify(productsListe));
-        console.log(productsListe);
+  btn_envoiPanier.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    }else{
-        productsListe = JSON.parse(localStorage.getItem("panier")) ;
-        productsListe.push(selectProduit);
-    
-        localStorage.setItem("panier",JSON.stringify(productsListe));
-
-        console.log(productsListe);
+    let selectProduit = {
+      imageUrl: data.imageUrl,
+      name: data.name,
+      _id: data._id,
+      quantite: 1,
+      price: data.price / 100,
     }
-      
-          
-     
-    });
-  
-  } 
- 
- 
+    let productsListe = [];
+
+    if (localStorage.getItem("panier") === null) {
+      //...
+
+      productsListe.push(selectProduit);
+
+      localStorage.setItem("panier", JSON.stringify(productsListe));
+      console.log(productsListe);
+
+    } else {
+      productsListe = JSON.parse(localStorage.getItem("panier"));
+      if (productsListe.filter(e => e._id === selectProduit._id).length == 0) {
+        productsListe.push(selectProduit);
+
+        localStorage.setItem("panier", JSON.stringify(productsListe));
+        console.log(productsListe);
+      }
+
+    
+    }
+
+
+
+  });
+
+}
+
+
